227319a22da2672f3c87a8f78db0f967f4cf29f9
Paola Perdomo <paolaperdomo079@gmail.com>
2024-12-20 10:58:21 +0100

    refactor: fix cloudinary class

--- src/main/java/com/example/Tech_Trends/cloudinary/CloudinaryProvider.java
+++ src/main/java/com/example/Tech_Trends/cloudinary/CloudinaryProvider.java
@@ -11,18 +11,33 @@ import java.util.Map;
 
 public class CloudinaryProvider {
 
-    private static Dotenv dotenv = Dotenv.load();
-    private static Cloudinary cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
     private static Map paramsImg = ObjectUtils.asMap(
             "use_filename", true,
             "unique_filename", false,
             "overwrite", true
     );
 
+    private static Cloudinary getCloudinaryInstance() {
+        String cloudinaryUrl;
+
+        try {
+            Dotenv dotenv = Dotenv.configure().load();
+            cloudinaryUrl = dotenv.get("CLOUDINARY_URL");
+        } catch (Exception e) {
+            cloudinaryUrl = System.getenv("CLOUDINARY_URL");
+        }
+
+        if (cloudinaryUrl == null || cloudinaryUrl.isEmpty()) {
+            throw new RuntimeException("CLOUDINARY_URL is not set");
+        }
+
+        return  new Cloudinary(cloudinaryUrl);
+    }
+
     public static Map<String, Object> uploadImage(String url) {
         Map<String, Object> uploadResult = Collections.emptyMap();
         try {
-            uploadResult = cloudinary.uploader().upload(url, paramsImg);
+            uploadResult = getCloudinaryInstance().uploader().upload(url, paramsImg);
         } catch (IOException e) {
             throw new TechTrendFailedUploadException(e.getMessage());
         }
